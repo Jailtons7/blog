cbf01aa276c70b8645d2f73fbf991006aeadfcfd
Jailton Silva <jss1@ifal.edu.br>
2021-10-17 17:55:32 -0300

    refact: initilizing app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 from project.settings import Settings
 
+migrate = Migrate()
+app = Flask(__name__)
+app.config.from_object(Settings)
+db = SQLAlchemy(app)
+migrate.init_app(app=app, db=db)
+ma = Marshmallow(app)
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Settings)
-    return app
+from app.models import users
+from app.models import blog
+from app import routes
