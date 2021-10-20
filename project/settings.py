549316342a1a18f3e0de778f2622bfb32f3a75b1
Jailton Silva <jss1@ifal.edu.br>
2021-10-20 11:27:07 -0300

    feat: adding settings to suport file upload

--- project/settings.py
+++ project/settings.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 from environs import Env
 
 env = Env()
@@ -6,6 +8,12 @@ env.read_env()
 
 class Settings:
     SECRET_KEY = env.str("SECRET_KEY")
+
+    PROJECT_ROOT = Path(__file__).parent.parent
+    UPLOAD_FOLDER = os.path.join(PROJECT_ROOT, 'media')
+
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+    MAX_CONTENT_LENGTH = 20 * 1024 * 1024  # 20 MB
 
     SQLALCHEMY_DATABASE_URI = env.str("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = env.bool("SQLALCHEMY_TRACK_MODIFICATIONS")
